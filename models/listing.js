68537045fbcdd4e3c5715b0af67b166cdaed8c74
Samarth Dhoble <samarthdhoble2005@gmail.com>
2025-06-08 15:16:54 +0530

    implemented middle ware for listing , when listing is deleted , all reviews for that listing is also deleted

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const Review = require('./review.js');
 
 const listingSchema = new Schema({
   title: {
@@ -40,6 +41,13 @@ const listingSchema = new Schema({
   }]
 
 });
+
+// MIDDLEWARE A LISTING IS DELETED, DELETE ALL REVIEWS ASSOCIATED WITH IT
+listingSchema.post('findOneAndDelete',async ( listing ) => { 
+  if(listing){
+     await Review.deleteMany({ _id  : {$in : listing.reviews}});
+  }
+})
 
 const Listing = mongoose.model('Listing', listingSchema);
 module.exports = Listing;
