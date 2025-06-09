89461706b47e40d86d24346b076d9ded6880c07c
Samarth Dhoble <samarthdhoble2005@gmail.com>
2025-06-09 22:44:22 +0530

    learnt sessions in websites

--- classroom/server.js
+++ classroom/server.js
@@ -3,33 +3,75 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const userRouter = require('./routers/user');
 const postRouter = require('./routers/post');
+const session = require('express-session');
 
-app.use(cookieParser())
+// app.use(cookieParser())
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get('/' , (req , res ) => {
   res.send('Welcome to the Classroom API');
 })
 
-app.use('/users' , userRouter);
-app.use('/posts' , postRouter);
+// app.use('/users' , userRouter);
+// app.use('/posts' , postRouter);
 
 
-app.get('/getcookie' , (req, res) => {
-  res.cookie('greet' , 'hello world');
-  res.send('Cookie has been set');
+// app.get('/getcookie' , (req, res) => {
+//   res.cookie('greet' , 'hello world');
+//   res.send('Cookie has been set');
+// })
+
+// app.get('/cookieuse' , (req , res) => {
+//   let { name = 'random' } = req.cookies;
+//   res.send(`Hello ${name}, welcome to the cookie use endpoint!`);
+// })
+
+// app.get('/showcookie' , (req, res) => {
+//   console.log(req.cookies);
+//   res.send(req.cookies);
+// })
+
+const sessionOptions = {
+  secret : "mysecretString" ,
+  resave : false ,
+  saveUninitialized : true
+}
+
+
+app.use(session(sessionOptions));
+
+app.get('/test' , (req , res) => {
+  res.send('Session Test Endpoint');
 })
 
-app.get('/cookieuse' , (req , res) => {
-  let { name = 'random' } = req.cookies;
-  res.send(`Hello ${name}, welcome to the cookie use endpoint!`);
+
+
+app.get('/register', (req ,res) => {
+  let { name = 'anonymous'  } = req.query;
+  req.session.name = name;
+  res.redirect('/hello');
 })
 
-app.get('/showcookie' , (req, res) => {
-  console.log(req.cookies);
-  res.send(req.cookies);
+
+app.get('/hello' , (req,res) => {
+  res.send(`Hello  ${req.session.name} ` );
 })
+
+// SESSION COUNT EXAMPLE -> 
+
+// app.get('/reqcount' , (req , res) => {
+
+//   if(req.session.count){
+//     req.session.count++;
+//   }else{
+//     req.session.count = 1;
+//   }
+ 
+
+//   res.send(`you sent a request ${req.session.count} times`)
+// })
+
 
 
 // ----------- Server Start -----------
